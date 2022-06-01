3939d721bd31b9ff37de35d91c106f1d02a42dcb
covesturtevant <csturtevant@battelleecology.org>
2022-06-01 13:22:28 -0600

    parameter inputs for put_folder action

--- .github/workflow_scripts/put_folder/put_folder.py
+++ .github/workflow_scripts/put_folder/put_folder.py
@@ -1,31 +1,45 @@
 #requirements
 # python 3.6.8
-# pip3 install python-pachyderm
-# pip3 install certifi
+# pip install python-pachyderm
+# pip install certifi
 
 
 import python_pachyderm
+from urllib.parse import urlparse
 import io
 import os
 
 print('Hello, the current working directory is:')
 print(os.getcwd())
 
-config = '''{
-  "v2": {
-    "active_context": "pachd1sandbox",
-    "contexts": {
-      "pachd1sandbox": {
-        "pachd_address": "grpcs://pachd1.sandbox.gcp.neoninternal.org:30650",
-        "cluster_deployment_id": "49fGuUbnKSwAee8Pdyfx2AEFnyUaZrK3"
-      }
-    }
-  }
-}'''
-client = python_pachyderm.Client.new_from_config(io.StringIO(config))
 
-# Put the updated files in Git into Pachyderm
-# Similar to `pachctl put file empty_files_test@master:/dir_a/data.txt`
-with client.commit("empty_files_test", "master") as commit:
-        python_pachyderm.put_files(client, "empty_files/prt/", commit, "/prt/")
+def setup_client():
+    pach_url = urlparse(os.environ["PACHYDERM_CLUSTER_URL"])
+    host = pach_url.hostname
+    port = pach_url.port
+    if port is None:
+        port = 80
+    if pach_url.scheme == "https":
+        tls = True
+    else:
+        tls = False
+    return python_pachyderm.Client(host=host, port=port, tls=tls)
+
+
+
+def main():
+    repo = os.environ["REPO"]
+    branch = os.environ["BRANCH"]
+    in_path = os.environ["IN_PATH"]
+    out_path = os.environ["OUT_PATH"]
+    # Setup connection to Pachyderm
+    client = setup_client()
+    # Put the updated files in Git into Pachyderm
+    with client.commit(repo, branch) as commit:
+            python_pachyderm.put_files(client, in_path, commit, out_path)
+    
+
+if __name__ == "__main__":
+    main()
+
 
